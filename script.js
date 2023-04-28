e02c62c7fdba24e0f382168db2b43f6ad213f4b0
Brehnden <bbirdy2018@gmail.com>
2023-04-27 22:10:36 -0400

    Add createGrid function to make any size grid

---
+++ script.js
@@ -0,0 +1,25 @@
+const div = document.createElement('div'),
+    container = document.querySelector('.container')
+let rows = []
+
+function createGrid(area) {
+    // Rows
+    let x = Math.sqrt(area)
+    for (let i = 0; i < x; i++) {
+        rows[i] = document.createElement('div');
+        rows[i].classList.add('row')
+        container.appendChild(rows[i]);
+
+        // Cells
+        for (let j = 0; j < x; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            rows[i].appendChild(cell);
+        }
+    }
+}
+
+
+// TEMP
+let area = 16*16;
+createGrid(area);
