2b92d3bc67227f78cbafcafa036df94c5f11f3bb
Brehnden <bbirdy2018@gmail.com>
2023-04-28 21:29:00 -0400

    Add listener for grid size buttons, deletes grid and creates new one. Few minor changes to make the buttons work correctly

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
 const div = document.createElement('div'),
-container = document.querySelector('.container'),
-btns = document.querySelectorAll('button')
+    container = document.querySelector('.container'),
+    btns = document.querySelectorAll('.color'),
+    sizeBtns = document.querySelectorAll('.size')
 
-let rows = [],
-pen = 'red'
+// let rows = [],
+let pen = 'red'
 
 function createGrid(area) {
     // Rows
     let x = Math.sqrt(area)
     for (let i = 0; i < x; i++) {
-        rows[i] = document.createElement('div');
-        rows[i].classList.add('row')
-        container.appendChild(rows[i]);
+        let row = document.createElement('div');
+        row.classList.add('row')
+        container.appendChild(row);
 
         // Cells
         for (let j = 0; j < x; j++) {
             let cell = document.createElement('div');
             cell.classList.add('cell');
-            rows[i].appendChild(cell);
+            row.appendChild(cell);
         }
     }
 }
 
-// TEMP
-let area = 16*16;
-createGrid(area);
+// Initial
+createGrid(16*16);
+draw()
 
-const cells = document.querySelectorAll('.cell') // Get Node List of all cells
-// Iterate through list, for every cell listen for mouse over and change bg color to pen color
-for (let cell of cells) {
-    cell.addEventListener('mouseover', ()=> {
-        cell.style.backgroundColor = pen;
-    })
+
+
+function draw() {
+    let cells = document.querySelectorAll('.cell') // Get Node List of all cells
+    // Iterate through list, for every cell listen for mouse over and change bg color to pen color
+    for (let cell of cells) {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = pen;
+        })
+    }
 }
 
-// Iterate through buttons, check each id, change pen color according
+
+// Iterate through color buttons, check each id, change pen color according
 for (let btn of btns) {
     btn.addEventListener('click', () => {
         let color = btn.getAttribute('id')
         pen = color
+    })
+}
+
+// Iterate through size buttons, check id, remove grid and create new one
+for (let btn of sizeBtns) {
+    btn.addEventListener('click', () => {
+        let rows = document.querySelectorAll('.row')
+        // Remove each row
+        rows.forEach((row) => {
+            container.removeChild(row)
+        })
+
+        let size = btn.getAttribute('id')
+        createGrid(size * size)
+        draw()
     })
 }
